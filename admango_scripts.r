0f658ca16463a743cc6fd59e58e7f2184e075d5c
Andrew Ball <74087125+one-orb@users.noreply.github.com>
2024-07-10 18:10:51 +0800

    update R scripts

---
+++ admango_scripts.r
@@ -0,0 +1,22 @@
+library("tidyverse")
+
+# TODO: create a script to adjust names when importing data.
+
+
+# single date conversion
+date <- mdy(<date>)
+
+# function to convert date ranges according to admango's output 
+# TODO: adjust to mutate the imported data frame?
+date_start <- mdy(str_sub(<date>, 1, 11))
+date_end <- mdy(str_sub(<date>, 1, 11))
+
+# gantt chart?
+ggplot(<data.frame>, aes(<date>, <ad_type or url>)) +
+  geom_line(group = <ad_type or url>, linewidth = 10, position = "jittery") +
+  labs (
+    x = "Date", y = "Type of ad",
+    title = "Something describing the table"
+    subtitle = "Data taken from **admango.com**"
+    caption = "Is there any data that has been ommitted?"
+  )
